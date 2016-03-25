1d6daf5b1e642414dad43428fa3bf6934780ee5b
Ben Raymond <ben@theraymonds.org>
2016-03-26 10:00:42 +1100

    fixed summary method for datavolley, added summary for dvlist

--- R/read_dv.R
+++ R/read_dv.R
@@ -134,31 +134,65 @@ read_dv <- function(filename,insert_technical_timeouts=TRUE,dowarn=FALSE,encodin
 }
 
 
-#' Print a simple summary of a datavolley object
+#' A simple summary of a volleyball match
 #'
 #' @param object datavolley: datavolley object as returned by read_dv()
 #' @param ... : additional arguments (currently these have no effect)
 #'
-#' @return print summary to console, and return the summary object invisibly
+#' @return list of summary items
 #'
 #' @seealso \code{\link{read_dv}}
+#' @examples
+#' \dontrun{
+#'   x <- read_dv(system.file("extdata/example_data.dvw",package="datavolley"))
+#'   summary(x)
+#' }
 #'
+#' @method summary datavolley
 #' @export
 summary.datavolley <- function(object,...) {
-    out <- sprintf("Match summary:\nDate: %s\nLeague: %s\n",object$meta$match$date,object$meta$match$league)
-    out <- sprintf("%sTeams: %s (%s/%s)\n       vs\n       %s (%s/%s)\n",out,object$meta$teams$team_name[1],object$meta$teams$coach[1],object$meta$teams$assistant[1],object$meta$teams$team_name[2],object$meta$teams$coach[2],object$meta$teams$assistant[2])
+    out <- list(date=object$meta$match$date,league=object$meta$match$league)
+    out$teams <- data.frame(team=object$meta$teams$team_name,coach=object$meta$teams$coach,assistant=object$meta$teams$assistant,sets_won=object$meta$teams$sets_won,stringsAsFactors=FALSE)
     temp <- object$meta$result$score_home_team>object$meta$result$score_visiting_team
-    out <- sprintf("%sResult: %d-%d (%s)\n",out,sum(temp),sum(!temp),paste(object$meta$result$score,collapse=", "))
-    out <- sprintf("%sDuration: %d minutes\n",out,sum(object$meta$result$duration))
+    out$set_scores <- object$meta$result$score
+    out$duration <- sum(object$meta$result$duration)
+    class(out) <- "summary.datavolley"
+    out
+}
+
+#' @rdname summary.datavolley
+#' @method print summary.datavolley
+#' @export
+print.summary.datavolley <- function(x,...) {
+    out <- sprintf("Match summary:\nDate: %s\nLeague: %s\n",x$date,x$league)
+    out <- sprintf("%sTeams: %s (%s/%s)\n       vs\n       %s (%s/%s)\n",out,x$teams$team[1],x$teams$coach[1],x$teams$assistant[1],x$teams$team[2],x$teams$coach[2],x$teams$assistant[2])
+    out <- sprintf("%sResult: %d-%d (%s)\n",out,x$teams$sets_won[1],x$teams$sets_won[2],paste(x$set_scores,collapse=", "))
+    out <- sprintf("%sDuration: %d minutes\n",out,x$duration)
     cat(out)
     invisible(out)
 }
 
+    
 
-dvlist_summary <- function(z) {
-    cat(sprintf("Number of matches: %d\n",length(z)))
-    temp <- range(ldply(z,function(q)as.Date(q$meta$match$date))$V1)
-    cat(sprintf("Match dates: %s to %s\n",temp[1],temp[2]))
+
+#' Summarize a list of volleyball matches
+#'
+#' @param z list: list of datavolley objects as returned by read_dv()
+#'
+#' @return named list with various summary indicators, including a competition ladder
+#'
+#' @seealso \code{\link{read_dv}}
+#'
+#' @examples
+#' \dontrun{
+#'   x <- read_dv(system.file("extdata/example_data.dvw",package="datavolley"))
+#'   summary(list(x,x)) ## same match duplicated twice, just for illustration purposes
+#' }
+#'
+#' @export
+dvlist_summary=function(z) {
+    out <- list(number_of_matches=length(z))
+    out$date_range <- range(ldply(z,function(q)as.Date(q$meta$match$date))$V1)
     temp <- as.character(sapply(z,function(q) q$meta$teams$team_name))
     teams <- as.data.frame(table(temp))
     names(teams) <- c("team_name","played")
@@ -167,7 +201,18 @@ dvlist_summary <- function(z) {
     teams$win_rate <- teams$won/teams$played
     names(teams)[1] <- "team"
     teams <- arrange(teams,team)
-    print(teams)
-    invisible(teams)
+    out$ladder <- teams
+    class(out) <- "summary.datavolleylist"
+    out
+}
+
+
+#' @rdname dvlist_summary
+#' @method print summary.datavolleylist
+#' @export
+print.summary.datavolleylist <- function(x,...) {
+    cat(sprintf("Number of matches: %d\n",x$number_of_matches))
+    cat(sprintf("Match dates: %s to %s\n",x$date_range[1],x$date_range[2]))
+    print(x$ladder)
 }
 
