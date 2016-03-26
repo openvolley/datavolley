1c7aeb20c7d57d9bc6fee99082a0b1566dd65b0f
Ben Raymond <ben@theraymonds.org>
2016-03-26 12:22:09 +1100

    roxygenized

--- R/datavolley.R
+++ R/datavolley.R
@@ -3,7 +3,7 @@
 #' @name datavolley
 #' @author Ben Raymond \email{ben@@untan.gl}
 #' @docType package
-#' @import plyr
+#' @import plyr assertthat
 #' @importFrom lubridate dmy_hms mdy_hms hms
 #' @importFrom digest digest
 #' @importFrom stringr str_match str_trim regex
