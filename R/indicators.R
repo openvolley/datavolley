5796651c17329a80cb7926f5e08b40882e6c78ce
Ben Raymond <ben@theraymonds.org>
2016-03-26 12:21:16 +1100

    optionally return match and play id

--- R/indicators.R
+++ R/indicators.R
@@ -3,8 +3,9 @@
 #' A break point in one in which the serving team wins the point. Break point rate is the fraction of points won by the serving team.
 #' 
 #' @param x data.frame: the plays component of a datavolley object, as returned by \code{read_dv()}
+#' @param return_id logical: include the match_id and play_id of all break points in the returned object?
 #'
-#' @return named list with components "ix" (logical indices of break points in the x object), "n" (number of break points in x), "rate" (break point rate from x)
+#' @return named list with components "ix" (logical indices of serves corresponding to break points in the x object), "n" (number of break points in x), "rate" (break point rate from x). If \code{return_id} is TRUE, also return a component "id" (a data.frame containing the match_id and play_id of all break points)
 #'
 #' @seealso \code{\link{read_dv}}
 #'
@@ -15,10 +16,15 @@
 #' }
 #'
 #' @export
-break_points=function(x) {
-    out <- list(ix=x$skill=="Serve" & !is.na(x$skill) & x$point_won_by==x$team)
-    out$n <- sum(out$ix,na.rm=TRUE)
+break_points=function(x,return_id=FALSE) {
+    assert_that(is.flag(return_id))
+    out <- list(ix=x$skill=="Serve" & x$point_won_by==x$team)
+    out$ix[is.na(out$ix)] <- FALSE
+    out$n <- sum(out$ix)
     out$rate <- out$n/sum(x$skill=="Serve",na.rm=TRUE)
+    if (return_id) {
+        out$id <- x[out$ix,c("match_id","play_id")]
+    }
     out
 }
 
