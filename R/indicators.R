8ef10312c361e6ea217cf103df127e018fa14e3b
Ben Raymond <ben@theraymonds.org>
2016-03-27 20:19:13 +1100

    break point changed to serve win point

--- R/indicators.R
+++ R/indicators.R
@@ -1,29 +1,29 @@
-#' Find break points
+#' Find serve win points
 #'
-#' A break point in one in which the serving team wins the point. Break point rate is the fraction of serves won by the serving team.
+#' Find points in which the serving team wins the point. Serve win rate is the fraction of serves won by the serving team.
 #' 
 #' @param x data.frame: the plays component of a datavolley object, as returned by \code{read_dv()}
-#' @param return_id logical: include the match_id and play_id of all break points in the returned object?
+#' @param return_id logical: include the match_id and point_id of all serve win points in the returned object?
 #'
-#' @return named list with components "ix" (logical indices of serves corresponding to break points in the x object), "n" (number of break points in x), "rate" (break point rate from x). If \code{return_id} is TRUE, also return a component "id" (a data.frame containing the match_id and play_id of all break points)
+#' @return named list with components "ix" (logical indices of serves corresponding to serve win points in the x object), "n" (number of serve win points in x), "rate" (serve win rate from x). If \code{return_id} is TRUE, also return a component "id" (a data.frame containing the match_id and point_id of all serve win points)
 #'
 #' @seealso \code{\link{read_dv}}
 #'
 #' @examples
 #' \dontrun{
 #'   x <- read_dv(system.file("extdata/example_data.dvw",package="datavolley"))
-#'   break_points(subset(x$plays,team=="Kamnik"))
+#'   serve_win_points(subset(x$plays,team=="Kamnik"))
 #' }
 #'
 #' @export
-break_points=function(x,return_id=FALSE) {
+serve_win_points=function(x,return_id=FALSE) {
     assert_that(is.flag(return_id))
     out <- list(ix=x$skill=="Serve" & x$point_won_by==x$team)
     out$ix[is.na(out$ix)] <- FALSE
     out$n <- sum(out$ix)
     out$rate <- out$n/sum(x$skill=="Serve",na.rm=TRUE)
     if (return_id) {
-        out$id <- x[out$ix,c("match_id","play_id")]
+        out$id <- x[out$ix,c("match_id","point_id")]
     }
     out
 }
