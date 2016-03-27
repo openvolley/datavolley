e95552b0be0ae594c0925fc850c646c592e286bd
Ben Raymond <ben@theraymonds.org>
2016-03-28 09:43:20 +1100

    coerce points_for, points_against to integer

--- R/read_dv.R
+++ R/read_dv.R
@@ -233,9 +233,8 @@ dvlist_summary=function(z) {
     teams <- suppressMessages(join(teams,ddply(temp,.(team),function(q)data.frame(won=sum(q$won_match)))))
     teams$win_rate <- teams$won/teams$played
     temp <- ldply(z,function(q){ s <- summary(q); s$sc <- colSums(s$set_scores); data.frame(team=s$teams$team,points_for=s$sc,points_against=s$sc[2:1])})
-    teams <- suppressMessages(join(teams,ddply(temp,.(team),function(q)data.frame(points_for=sum(q$points_for),points_against=sum(q$points_against)))))
+    teams <- suppressMessages(join(teams,ddply(temp,.(team),function(q)data.frame(points_for=as.integer(sum(q$points_for)),points_against=as.integer(sum(q$points_against))))))
     teams$points_ratio <- teams$points_for/teams$points_against
-    ##names(teams)[1] <- "team"
     teams <- arrange(teams,team)
     out$ladder <- teams
     class(out) <- "summary.datavolleylist"
