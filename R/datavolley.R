984efe636b3138eed5d1723d86c1dc6bebc0f558
Ben Raymond <ben@theraymonds.org>
2016-05-21 18:13:19 +1000

    updated imports for R 3.3

--- R/datavolley.R
+++ R/datavolley.R
@@ -10,6 +10,9 @@
 #' @importFrom digest digest
 #' @importFrom stringr str_match str_trim regex fixed str_replace_all
 #' @importFrom data.table fread
+#' @importFrom stats na.omit
+#' @importFrom utils adist read.table
+
 NULL
 
 
