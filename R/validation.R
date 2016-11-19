4c741c2d92c2fb740773ad2fff618d9bea98bb51
Ben Raymond <ben@theraymonds.org>
2016-11-19 16:28:04 +1100

    use standard subsetting to avoid check() warnings

--- R/validation.R
+++ R/validation.R
@@ -20,22 +20,23 @@ validate_dv <- function(x) {
     plays <- plays(x)
     out <- data.frame(file_line_number=integer(),message=character(),file_line=character(),stringsAsFactors=FALSE)
     ## find front-row players for each attack
-    attacks <- subset(plays,skill %eq% "Attack")
+    attacks <- plays[plays$skill %eq% "Attack",]
     temp <- ldply(1:nrow(attacks),function(z) {fcols <- if (attacks$home_team[z]==attacks$team[z]) c("home_p2","home_p3","home_p4") else c("visiting_p2","visiting_p3","visiting_p4"); data.frame(attacker2=attacks[z,fcols[1]],attacker3=attacks[z,fcols[2]],attacker4=attacks[z,fcols[3]])})
     attacks$attacker2 <- temp$attacker2
     attacks$attacker3 <- temp$attacker3
     attacks$attacker4 <- temp$attacker4
 
     ## front-row attacking player isn't actually in front row
-    chk <- subset(attacks,start_zone %in% c(2,3,4) & player_number!=attacker2 & player_number!=attacker3 & player_number!=attacker4)
+    chk <- attacks[attacks$start_zone %in% c(2,3,4) & attacks$player_number!=attacks$attacker2 & attacks$player_number!=attacks$attacker3 & attacks$player_number!=attacks$attacker4,]
+    
     if (nrow(chk)>0)
         out <- rbind(out,data.frame(file_line_number=chk$file_line_number,message="player making a front row attack is not in the front row",file_line=x$raw[chk$file_line_number],stringsAsFactors=FALSE))
     out
 
     ## player not in recorded rotation making a play (other than by libero)
-    liberos_v <- subset(x$meta$players_v,grepl("L",special_role))$number
-    liberos_h <- subset(x$meta$players_h,grepl("L",special_role))$number
-    pp <- subset(plays,skill %in% c("Serve","Attack","Block","Dig","Freeball","Reception","Set"))
+    liberos_v <- x$meta$players_v$number[grepl("L",x$meta$players_v$special_role)] ##subset(x$meta$players_v,grepl("L",special_role))$number
+    liberos_h <- x$meta$players_h$number[grepl("L",x$meta$players_h$special_role)] ##subset(x$meta$players_h,grepl("L",special_role))$number
+    pp <- plays[plays$skill %in% c("Serve","Attack","Block","Dig","Freeball","Reception","Set"),]
     pp$labelh <- "home_team"
     pp$labelv <- "visiting_team"
     temp <- ldply(1:nrow(pp),function(z) {fcols <- if (pp$home_team[z] %eq% pp$team[z]) c("home_p1","home_p2","home_p3","home_p4","home_p5","home_p6","labelh") else c("visiting_p1","visiting_p2","visiting_p3","visiting_p4","visiting_p5","visiting_p6","labelv"); out <- pp[z,fcols]; names(out) <- c(paste0("player",1:6),"which_team"); out })
