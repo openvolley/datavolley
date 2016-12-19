e41008554dd137e081c9cc47755c63908a2d97a5
Ben Raymond <ben@theraymonds.org>
2016-12-19 20:09:05 +1100

    adjust test for new evaluation outputs

--- tests/testthat/test_evaluations.R
+++ tests/testthat/test_evaluations.R
@@ -4,7 +4,8 @@ test_that("skill_evaluation_decoder generally functions as expected", {
     sed <- skill_evaluation_decoder()
     expect_error(sed())
     expect_error(sed("X"))
-    expect_error(sed("X","$"))
+    expect_error(sed("XX","$"))
+    expect_equal(sed("X","$"),"Unknown skill: X")
 
     expect_equal(sed("E","#"),"Perfect")
     expect_equal(sed("E","x"),"unknown set evaluation")    
