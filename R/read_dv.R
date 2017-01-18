43a656893f7e0529047065566d82bd5aa26008e8
Ben Raymond <ben@theraymonds.org>
2017-01-18 17:39:37 +1100

    suppress warnings on POSIX conversion of time column

--- R/read_dv.R
+++ R/read_dv.R
@@ -186,7 +186,12 @@ read_dv <- function(filename,insert_technical_timeouts=TRUE,do_warn=FALSE,do_tra
     ## turn plays times (character) into POSIXct
     temp <- paste(format(as.Date(out$meta$match$date)),out$plays$time,sep=" ")
     temp[out$plays$time=="" | is.na(out$plays$time)] <- NA
-    out$plays$time <- ymd_hms(temp)
+    suppressWarnings(out$plays$time <- ymd_hms(temp))
+    ##if (all(is.na(out$plays$time))) {
+    ##    ## maybe match date failed to parse? try
+    ##    suppressWarnings(out$plays$time <- hms(temp))
+    ## except this gives objects of class period, not POSIXct - hold off on this for now
+    ##}        
     
     ## add point_id - an identifier of each point. One point may consist of multiple attacks or other actions. Timeouts get assigned to their own "point", but other non-play rows may get assigned as part of a point.
     pid <- 0
