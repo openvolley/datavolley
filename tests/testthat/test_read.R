8a1ad336918e782f1e6deff8a88c6d67e7f7f7a1
Ben Raymond <ben@theraymonds.org>
2018-05-11 12:58:33 +1000

    adjust tests for message() not cat()

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,7 +1,7 @@
 context("dvw file reader")
 
 test_that("text encoding is guessed correctly", {
-    expect_output(read_dv(dv_example_file(1), insert_technical_timeouts=FALSE, verbose=TRUE), "Using text encoding: UTF-8$")
-    tmp <- capture_output(read_dv(dv_example_file(2),insert_technical_timeouts=FALSE, verbose=TRUE))
-    expect_true(grepl("Using text encoding: windows-1250", tmp))
+    expect_message(read_dv(dv_example_file(1), insert_technical_timeouts=FALSE, verbose=TRUE), "Using text encoding: UTF-8")
+    tmp <- capture_messages(read_dv(dv_example_file(2),insert_technical_timeouts=FALSE, verbose=TRUE))
+    expect_true(any(grepl("Using text encoding: windows-1250", tmp)))
 })
