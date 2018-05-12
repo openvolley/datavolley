020219539eed4ba9323eed3aa3bbe21868cb5d43
Ben Raymond <ben@theraymonds.org>
2018-05-12 10:03:55 +1000

    add test for file with truncated lines

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -5,3 +5,9 @@ test_that("text encoding is guessed correctly", {
     tmp <- capture_messages(read_dv(dv_example_file(2),insert_technical_timeouts=FALSE, verbose=TRUE))
     expect_true(any(grepl("Using text encoding: windows-1250", tmp)))
 })
+
+test_that("file reading handles files with truncated lines", {
+    x1 <- read_dv(dv_example_file(1))
+    xt <- read_dv(system.file("extdata/testing_truncated.dvw",package="datavolley"))
+    expect_equal(nrow(x1$plays), nrow(xt$plays))
+})
