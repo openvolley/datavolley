db154f5a98a3a05c48b0b3fac73d1df72e0d5ff8
Ben Raymond <ben@theraymonds.org>
2018-05-26 09:40:03 +1000

    test added

---
+++ tests/testthat/test_names.R
@@ -0,0 +1,9 @@
+context("name checking and cleaning")
+
+test_that("check_player_names works", {
+    x <- read_dv(dv_example_file(1), insert_technical_timeouts=FALSE)
+    cpn <- check_player_names(x)
+    ## expect only two similar names here, i.e. one row
+    expect_equal(nrow(cpn), 1)
+    expect_identical(sort(c(cpn$player1, cpn$player2)), c("ANJA HRIBERNIK 96", "ANJA HRIBERNIK 97"))
+})
