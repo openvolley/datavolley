ecb16c43bfc2ce4ba50872c515824f5d0f0c1693
Ben Raymond <ben@theraymonds.org>
2019-04-26 08:06:41 +1000

    cope with no end-of-set markers

--- R/read_dv.R
+++ R/read_dv.R
@@ -268,13 +268,17 @@ read_dv <- function(filename, insert_technical_timeouts=TRUE, do_warn=FALSE, do_
         out$plays[,paste0("home_player_id",thisp)] <- get_player_id(rep("*",nrow(out$plays)),out$plays[,paste0("home_p",thisp)],out$meta)
     for (thisp in team_player_num)
         out$plays[,paste0("visiting_player_id",thisp)] <- get_player_id(rep("a",nrow(out$plays)),out$plays[,paste0("visiting_p",thisp)],out$meta)
-        
+
     ## add set number
-    if (!any(out$plays$end_of_set)) stop("could not find any end-of-set markers (e.g. \"**1set\") in the input file")
-    out$plays$set_number <- NA
-    temp <- c(0,which(out$plays$end_of_set))
-    for (si in 2:length(temp)) {
-        out$plays$set_number[(temp[si-1]+1):(temp[si]-1)] <- (si-1)
+    if (!any(out$plays$end_of_set)) {
+        warning("could not find any end-of-set markers (e.g. \"**1set\") in the input file")
+        out$plays$set_number <- 1L
+    } else {
+        out$plays$set_number <- NA
+        temp <- c(0L, which(out$plays$end_of_set))
+        for (si in 2:length(temp)) {
+            out$plays$set_number[(temp[si-1]+1):(temp[si]-1)] <- (si-1)
+        }
     }
 
     ## technical timeouts
