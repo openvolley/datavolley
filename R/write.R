669e5bf14a233898f93092ab546e8025b55a1630
Ben Raymond <ben@theraymonds.org>
2020-02-19 19:43:45 +1100

    first cut at dv_write function

---
+++ R/write.R
@@ -0,0 +1,189 @@
+#' Write a datavolley object to dvw file
+#'
+#' Note that this is really rather experimental, and you probably shouldn't use it yet. Once complete, this function will allow a datavolley file to be read in via \code{\link{read_dv}}, modified by the user, and then rewritten back to a datavolley file.
+#' At this stage, most modifications to the datavolley object should make it back into the rewritten file. However, the scouted code (in the \code{code} column) is NOT yet updated to reflect changes that might have been made to other columns in the datavolley object.
+#'
+#' @param x datvolley: a datavolley object as returned by \code{\link{read_dv}}
+#' @param file string: the filename to write to. If not supplied, no file will be written but the dvw content will be returned
+#' @param text_encoding string: the text encoding to use
+#'
+#' @return The dvw file contents as a character vector (invisibly)
+#'
+#' @seealso \code{\link{read_dv}}
+#'
+#' @examples
+#' \dontrun{
+#'   x <- read_dv(dv_example_file())
+#'   outfile <- tempfile()
+#'   dv_write(x, outfile)
+#' }
+#' @export
+dv_write <- function(x, file, text_encoding = "UTF-8") {
+    if (!inherits(x, "datavolley")) stop("x must be a datavolley object")
+    out <- c(dvw_header(x, text_encoding = text_encoding),
+             dvw_match(x, text_encoding = text_encoding),
+             dvw_teams(x, text_encoding = text_encoding),
+             dvw_more(x, text_encoding = text_encoding),
+             dvw_comments(x, text_encoding = text_encoding),
+             dvw_set(x, text_encoding = text_encoding),
+             dvw_players_h(x, text_encoding = text_encoding),
+             dvw_players_v(x, text_encoding = text_encoding),
+             dvw_attack_combos(x, text_encoding = text_encoding),
+             dvw_setter_calls(x, text_encoding = text_encoding),
+             dvw_winning_symbols(x, text_encoding = text_encoding),
+             "[3RESERVE]",
+             dvw_video(x, text_encoding = text_encoding),
+             dvw_scout(x, text_encoding = text_encoding))
+    if (!missing(file)) {
+        outf <- file(file, "wt") ## encoding??
+        writeLines(out, outf)
+        close(outf)
+    }
+    invisible(out)
+}
+
+not_null_or <- function(z, or) if (is.null(z)) or else z
+
+df2txt <- function(z) {
+    ## change any logical cols to char "True" "False"
+    logical2char <- function(w) {
+        out <- as.character(w)
+        out[which(w)] <- "True"
+        out[which(!w)] <- "False"
+        out
+    }
+    findlogicalcols <- function(w) {
+        vapply(seq_len(ncol(w)), function(ci) is.logical(w[[ci]]) & !all(is.na(w[[ci]])), FUN.VALUE = TRUE)
+    }
+    for (lc in which(findlogicalcols(z))) z[[lc]] <- logical2char(z[[lc]])
+    capture.output(data.table::fwrite(z, sep = ";", col.names = FALSE, row.names = FALSE, quote = FALSE, na = ""))
+}
+
+## helper for simple sections that just require a dump of the existing section
+sect2txt <- function(sect, component, outhdr, trailing_col = FALSE) {
+    if (is.null(sect)) stop("missing the ", component, " component of the input object")
+    if (trailing_col) sect[, ncol(sect)+1] <- NA_character_
+    c(outhdr, df2txt(sect))
+}
+
+dvw_video <- function(x, text_encoding) {
+    mm <- x$meta$video
+    if (is.null(mm)) {
+        ## this section isn't always present
+        NULL
+    } else if (nrow(mm) < 1) {
+        ## just the section header but nothing more
+        "[3VIDEO]"
+    } else {
+        ## a row like Camera0=C:\\Video\\A vs B.mp4
+        ## presumably can be multiple rows?
+        c("[3VIDEO]", paste(mm$camera, mm$file, sep = "="))
+    }
+}
+
+## metadata sections
+dvw_winning_symbols <- function(x, text_encoding) {
+    mm <- x$meta$winning_symbols
+    if (is.null(mm)) stop("missing the meta$winning_symbols component of the input object")
+    c("[3WINNINGSYMBOLS]", mm)
+}
+
+dvw_setter_calls <- function(x, text_encoding) sect2txt(x$meta$sets, "meta$sets", "[3SETTERCALL]")
+
+dvw_attack_combos <- function(x, text_encoding) sect2txt(x$meta$attacks, "meta$attacks", "[3ATTACKCOMBINATION]")
+
+dvw_players_v <- function(x, text_encoding) {
+    tmp <- x$meta$players_v[, setdiff(names(x$meta$players_v), "name")]
+    tmp$role <- roles_str2int(tmp$role)
+    sect2txt(tmp, "meta$players_v", "[3PLAYERS-V]")
+}
+
+dvw_players_h <- function(x, text_encoding) {
+    tmp <- x$meta$players_h[, setdiff(names(x$meta$players_h), "name")]
+    tmp$role <- roles_str2int(tmp$role)
+    sect2txt(tmp, "meta$players_h", "[3PLAYERS-H]")
+}
+
+dvw_set <- function(x, text_encoding) {
+    tmp <- sect2txt(x$meta$result[, setdiff(names(x$meta$result), c("score_home_team", "score_visiting_team"))], "meta$result", "[3SET]")
+    ## note that input might have unplayed sets, e.g.
+    ## True;;;;;;
+    ## as extra lines, but these have been stripped during read_dv
+    c(tmp, rep("True;;;;;;", 6-length(tmp)))
+}
+
+dvw_comments <- function(x, text_encoding) sect2txt(x$meta$comments, "meta$comments", "[3COMMENTS]")
+
+dvw_more <- function(x, text_encoding) c(sect2txt(x$meta$more, "meta$more ", "[3MORE]"), ";0;0;")
+
+dvw_teams <- function(x, text_encoding) {
+    tmp <- x$meta$teams[, setdiff(names(x$meta$teams), c("home_away_team", "won_match"))]
+    sect2txt(tmp, "meta$teams", "[3TEAMS]")
+}
+
+dvw_match <- function(x, text_encoding) {
+    ## [3MATCH]
+    ## @@match_date@@;@@match_time@@;@@season@@;@@league@@;;;;;UTF-8;1;@@cones_zones@@;0;
+    ## ;;12345;;;;;;
+    ## not sure what the final line is. The 12345 is a dummy value, obviously
+    mm <- x$meta$match
+    if (is.null(mm)) stop("missing the meta$match component of the input object")
+    if (!missing(text_encoding)) mm$text_encoding <- text_encoding
+    c("[3MATCH]", df2txt(mm), ";;12345;;;;;;")
+}
+
+dvw_header <- function(x, text_encoding) {
+    fm <- x$file_meta
+    if (is.null(fm)) stop("missing file_meta component of the input object")
+    tdnow <- format(Sys.time(), "%d/%m/%Y %H.%M.%S")
+    c("[3DATAVOLLEYSCOUT]",
+      paste0("FILEFORMAT: ", not_null_or(fm$fileformat, "2.0")),
+      paste0("GENERATOR-DAY: ", not_null_or(fm$generator_day, tdnow)),
+      paste0("GENERATOR-IDP: ", not_null_or(fm$generator_idp, "")),
+      paste0("GENERATOR-PRG: ", not_null_or(fm$generator_prg, "")),
+      paste0("GENERATOR-REL: ", not_null_or(fm$generator_rel, "")),
+      paste0("GENERATOR-VER: ", not_null_or(fm$generator_ver, "")),
+      paste0("GENERATOR-NAM: ", not_null_or(fm$generator_nam, "")),
+      paste0("LASTCHANGE-DAY: ", tdnow),
+      paste0("LASTCHANGE-IDP: datavolley"),
+      paste0("LASTCHANGE-PRG: datavolley-R"),
+      paste0("LASTCHANGE-REL: ", packageVersion("datavolley")),
+      paste0("LASTCHANGE-VER: "),
+      paste0("LASTCHANGE-NAM: "))
+}
+
+dvw_scout <- function(x, text_encoding = text_encoding) {
+    xp <- plays(x)
+    if (is.null(xp)) stop("missing the plays component of the input object")
+    ## drop TTOs
+    xp <- xp[!xp$skill %eq% "Technical timeout", ]
+    ## if the data have been modified then the original code may need changing
+    xp$code_modified <- vapply(seq_len(nrow(xp)), function(z) line2code(xp[z, ]), FUN.VALUE = "")
+    xp$na_col <- NA_character_ ## for unused output columns
+    ## some reformatting
+    xp$time <- format(xp$time, "%H.%M.%S")
+    ## setter position uses 0 not missing when unknown
+    xp$home_setter_position[is.na(xp$home_setter_position)] <- 0
+    xp$visiting_setter_position[is.na(xp$visiting_setter_position)] <- 0
+    nms <- c("code_modified", "na_col", "na_col", "na_col", ## cols 1-4
+             "start_coordinate", "mid_coordinate", "end_coordinate", ## cols 5-7
+             "time", ## col 8, HH.MM.SS format
+             "set_number", ## col 9
+             "home_setter_position", ## 10
+             "visiting_setter_position", # 11
+             "na_col", ##names(x)[12] <- ## is always 1 or NA?
+             "video_time", ## 13
+             "na_col", ## 14 need to check this one
+             paste("home_p", 1:6, sep = ""), ## 15-20, home team, court positons 1-6, entries are player numbers
+             paste("visiting_p", 1:6, sep = ""), ## 21-26, same for visiting team
+             "na_col" ## always a trailing ;
+             )
+    c("[3SCOUT]", df2txt(xp[, nms]))
+}
+
+## rebuild the scouted code from the plays dataframe row
+## for now, just use the existing code
+line2code <- function(z) {
+    z$code
+}
+
---
+++ tests/testthat/test_write.R
@@ -0,0 +1,38 @@
+context("dvw file writer")
+
+test_that("dv_write behaves", {
+    ## read a file
+    x0 <- read_dv(dv_example_file(1))
+    ## write it
+    outfile <- tempfile()
+    dv_write(x0, file = outfile)
+    ## read the one we just wrote
+    x1 <- read_dv(outfile)
+
+    ## it should be identical to the original one
+    ##  except for some known differences
+    ## fix the things that we know will be different
+    x0$raw <- NULL
+    x1$raw <- NULL
+    x1$file_meta$lastchange_day <- x0$file_meta$lastchange_day
+    x1$file_meta$lastchange_idp <- x0$file_meta$lastchange_idp
+    x1$file_meta$lastchange_prg <- x0$file_meta$lastchange_prg
+    x1$file_meta$lastchange_rel <- x0$file_meta$lastchange_rel
+    x1$file_meta$lastchange_ver <- x0$file_meta$lastchange_ver
+    x1$file_meta$lastchange_nam <- x0$file_meta$lastchange_nam
+
+    x0$messages$file_line <- NA_character_
+    x1$messages$file_line <- NA_character_
+
+    x0$meta$match$text_encoding <- NA_character_
+    x1$meta$match$text_encoding <- NA_character_
+
+    x1$meta$match_id <- x0$meta$match_id
+    x1$meta$filename <- x0$meta$filename
+    x1$plays$match_id <- x1$meta$match_id
+    ## x1 will have had an (empty) [3VIDEO] section inserted, but x0 does not have it
+    ## that doesn't really matter but the file line numbers will be off by one because of it
+    x1$plays$file_line_number[which(x1$plays$file_line_number >= 118)] <- x1$plays$file_line_number[which(x1$plays$file_line_number >= 118)]-1L
+    x1$messages$file_line_number[which(x1$messages$file_line_number >= 118)] <- x1$messages$file_line_number[which(x1$messages$file_line_number >= 118)]-1L
+    expect_equal(x0, x1)
+})
