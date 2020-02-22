980531fbac932c463cc9102f6e61cefc790ae67a
Ben Raymond <ben@theraymonds.org>
2020-02-23 10:47:34 +1100

    refactor write test code

--- tests/testthat/test_write.R
+++ tests/testthat/test_write.R
@@ -1,33 +1,41 @@
 context("dvw file writer")
 
 test_that("dv_write behaves", {
-    ## read a file
-    x0 <- read_dv(dv_example_file(1))
-    ## write it
-    outfile <- tempfile()
-    dv_write(x0, file = outfile)
-    ## read the one we just wrote
-    x1 <- read_dv(outfile)
+    expect_dvws_equal <- function(dvw1, dvw2) {
+        ## dvw2 should be identical to dvw1 except for some known differences
+        ## fix the things that we know will be different
+        if (!any(dvw1$raw %eq% "[3VIDEO]")) {
+            vididx <- which(dvw2$raw %eq% "[3VIDEO]")
+            ## x1 will have an (empty) [3VIDEO] section inserted, but x0 may not have it
+            ## that doesn't really matter but the file line numbers will be off by one because of it
+            dvw2$plays$file_line_number[which(dvw2$plays$file_line_number >= vididx)] <- dvw2$plays$file_line_number[which(dvw2$plays$file_line_number >= vididx)]-1L
+            dvw2$messages$file_line_number[which(dvw2$messages$file_line_number >= vididx)] <- dvw2$messages$file_line_number[which(dvw2$messages$file_line_number >= vididx)]-1L
+        }
+        dvw1$raw <- NULL
+        dvw2$raw <- NULL
+        dvw2$file_meta$lastchange_day <- dvw1$file_meta$lastchange_day
+        dvw2$file_meta$lastchange_idp <- dvw1$file_meta$lastchange_idp
+        dvw2$file_meta$lastchange_prg <- dvw1$file_meta$lastchange_prg
+        dvw2$file_meta$lastchange_rel <- dvw1$file_meta$lastchange_rel
+        dvw2$file_meta$lastchange_ver <- dvw1$file_meta$lastchange_ver
+        dvw2$file_meta$lastchange_nam <- dvw1$file_meta$lastchange_nam
 
-    ## it should be identical to the original one
-    ##  except for some known differences
-    ## fix the things that we know will be different
-    x0$raw <- NULL
-    x1$raw <- NULL
-    x1$file_meta$lastchange_day <- x0$file_meta$lastchange_day
-    x1$file_meta$lastchange_idp <- x0$file_meta$lastchange_idp
-    x1$file_meta$lastchange_prg <- x0$file_meta$lastchange_prg
-    x1$file_meta$lastchange_rel <- x0$file_meta$lastchange_rel
-    x1$file_meta$lastchange_ver <- x0$file_meta$lastchange_ver
-    x1$file_meta$lastchange_nam <- x0$file_meta$lastchange_nam
+        dvw1$meta$match$text_encoding <- NA_character_
+        dvw2$meta$match$text_encoding <- NA_character_
 
-    x0$meta$match$text_encoding <- NA_character_
-    x1$meta$match$text_encoding <- NA_character_
-
-    x1$meta$filename <- x0$meta$filename
-    ## x1 will have had an (empty) [3VIDEO] section inserted, but x0 does not have it
-    ## that doesn't really matter but the file line numbers will be off by one because of it
-    x1$plays$file_line_number[which(x1$plays$file_line_number >= 118)] <- x1$plays$file_line_number[which(x1$plays$file_line_number >= 118)]-1L
-    x1$messages$file_line_number[which(x1$messages$file_line_number >= 118)] <- x1$messages$file_line_number[which(x1$messages$file_line_number >= 118)]-1L
-    expect_equal(x0, x1)
+        dvw2$meta$filename <- dvw1$meta$filename
+        expect_equal(dvw1, dvw2)
+    }
+    test_read_write_dvw <- function(filename) {
+        ## read a file
+        x0 <- read_dv(filename)
+        ## write it
+        outfile <- tempfile()
+        dv_write(x0, file = outfile)
+        ## read the one we just wrote
+        x1 <- read_dv(outfile)
+        expect_dvws_equal(x0, x1)
+    }
+    test_read_write_dvw(dv_example_file(1))
+    expect_warning(test_read_write_dvw(dv_example_file(2)), "missing the meta\\$(attacks|sets) component", all = TRUE)
 })
