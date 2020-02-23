b449a3670980e880f99a8772f31394441ed3bbcb
Ben Raymond <ben@theraymonds.org>
2020-02-23 11:50:41 +1100

    tweak write test

--- tests/testthat/test_write.R
+++ tests/testthat/test_write.R
@@ -5,14 +5,21 @@ test_that("dv_write behaves", {
         ## dvw2 should be identical to dvw1 except for some known differences
         ## fix the things that we know will be different
         if (!any(dvw1$raw %eq% "[3VIDEO]")) {
+            ## dvw2 will have an (empty) [3VIDEO] section inserted if dvw1 did not have one
             vididx <- which(dvw2$raw %eq% "[3VIDEO]")
-            ## x1 will have an (empty) [3VIDEO] section inserted, but x0 may not have it
+            dvw2$raw <- dvw2$raw[-vididx]
             ## that doesn't really matter but the file line numbers will be off by one because of it
             dvw2$plays$file_line_number[which(dvw2$plays$file_line_number >= vididx)] <- dvw2$plays$file_line_number[which(dvw2$plays$file_line_number >= vididx)]-1L
             dvw2$messages$file_line_number[which(dvw2$messages$file_line_number >= vididx)] <- dvw2$messages$file_line_number[which(dvw2$messages$file_line_number >= vididx)]-1L
         }
-        dvw1$raw <- NULL
-        dvw2$raw <- NULL
+        if (TRUE) {
+            ## don't compare raw yet
+            dvw1$raw <- NULL
+            dvw2$raw <- NULL
+        } else {
+            ## drop trailing empty line in dvw1 if it exists
+            if (!nzchar(dvw1$raw[length(dvw1$raw)])) dvw1$raw <- dvw1$raw[-length(dvw1$raw)]
+        }
         dvw2$file_meta$lastchange_day <- dvw1$file_meta$lastchange_day
         dvw2$file_meta$lastchange_idp <- dvw1$file_meta$lastchange_idp
         dvw2$file_meta$lastchange_prg <- dvw1$file_meta$lastchange_prg
