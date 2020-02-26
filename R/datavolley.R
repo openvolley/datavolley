f60297e67fec7247e6721bcdb177bd332590de96
Ben Raymond <ben@theraymonds.org>
2020-02-27 07:23:31 +1100

    use readr::read_delim not read_csv2

--- R/datavolley.R
+++ R/datavolley.R
@@ -12,7 +12,6 @@
 #' @importFrom data.table fread
 #' @importFrom digest digest
 #' @importFrom graphics lines par plot text
-#' @importFrom readr read_csv2 locale
 #' @importFrom stringi stri_trans_general stri_enc_detect2
 #' @importFrom stringr str_match str_trim regex fixed str_replace_all str_to_title str_locate str_sub
 #' @importFrom stats na.omit runif setNames
